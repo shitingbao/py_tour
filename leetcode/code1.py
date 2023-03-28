9419b1efac963b98895db8f065c53fd65c82c164
shitingbao <871258317@qq.com>
2023-03-28 17:59:47 +0800

    feat: dictionary tree

--- leetcode/code1.py
+++ leetcode/code1.py
@@ -1,1 +1,55 @@
 # start 1
+class Node:
+    children = {}
+    isWord = False
+
+class StreamChecker(object):
+    def __init__(self, words):
+        """
+        :type words: List[str]
+        """
+        self.word = ""
+        self.root = Node()
+        for word in words:
+            node = self.root
+            word = word[::-1]
+            for w in word:
+                if w not in node.children:
+                    node.children[w] = Node()
+                node = node.children[w]
+            node.isWord = True
+            del node
+    
+    def query(self, letter):
+        """
+        :type letter: str
+        :rtype: bool
+        """
+        self.word = letter + self.word 
+        word = self.word
+        if len(word)==0:
+            return False
+        
+        node = self.root
+        for w in word:
+            if w not in node.children:
+                return False
+            node = node.children[w]
+            if node.isWord:
+                return True
+        return False
+    
+st = StreamChecker(["cd","f","kl"])
+# ["a"],["b"],["c"],["d"],["e"],["f"],["g"],["h"],["i"],["j"],["k"],["l"]
+print("query a:",st.query("a"),st.word)
+print("query b:",st.query("b"),st.word)
+print("query c:",st.query("c"),st.word)
+print("query d:",st.query("d"),st.word)
+print("query e:",st.query("e"),st.word)
+print("query f:",st.query("f"),st.word)
+print("query g:",st.query("g"),st.word)
+print("query h:",st.query("h"),st.word)
+print("query i:",st.query("i"),st.word)
+print("query j:",st.query("j"),st.word)
+print("query k:",st.query("k"),st.word)
+print("query l:",st.query("l"),st.word)
--- leetcode/code2.py
+++ leetcode/code2.py
@@ -1,1 +1,42 @@
-# start 2
+class TrieNode:
+    def __init__(self):
+        self.children = {}
+        self.is_end_of_word = False
+
+class Trie:
+    def __init__(self, words):
+        self.root = TrieNode()
+        for word in words:
+            self.insert(word)
+
+    def insert(self, word):
+        current_node = self.root
+        for char in word:
+            if char not in current_node.children:
+                current_node.children[char] = TrieNode()
+            current_node = current_node.children[char]
+        current_node.is_end_of_word = True
+
+    def search(self, word):
+        current_node = self.root
+        for char in word:
+            if char not in current_node.children:
+                return False
+            current_node = current_node.children[char]
+        return current_node.is_end_of_word
+
+    def starts_with(self, prefix):
+        current_node = self.root
+        words = []
+        for char in prefix:
+            if char not in current_node.children:
+                return []
+            current_node = current_node.children[char]
+        self._dfs(current_node, prefix, words)
+        return words
+
+    def _dfs(self, node, prefix, words):
+        if node.is_end_of_word:
+            words.append(prefix)
+        for char in node.children:
+            self._dfs(node.children[char], prefix + char, words)
