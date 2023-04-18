3b434a31c52fe7bb5f045f9cf8466a4f016fdee5
shitingbao <871258317@qq.com>
2023-04-18 11:09:39 +0800

    perf: code

--- leetcode/code4.py
+++ leetcode/code4.py
@@ -1,9 +1,8 @@
 class Solution(object):
     def countDaysTogether(self, arriveAlice, leaveAlice, arriveBob, leaveBob):
-        if arriveAlice < leaveBob and leaveAlice < arriveBob:
+        if arriveAlice > leaveBob or leaveAlice < arriveBob:
             return 0
-
-        if arriveBob > arriveAlice and leaveAlice > leaveBob:
+        if arriveBob >= arriveAlice and leaveAlice >= leaveBob:
             return self.getDay(arriveBob, leaveBob)
         if arriveAlice > arriveBob and leaveBob > leaveAlice:
             return self.getDay(arriveAlice, leaveAlice)
@@ -19,15 +18,18 @@ class Solution(object):
             day[idx+1] = month
         start = startDay.split("-")
         stop = stopDay.split("-")
-        if start[0] == stop[0]:
-            return stop[1]-start[1]+1
-        print(start, stop)
-        return 0
+        startNum0, startNum1 = int(start[0]), int(start[1])
+        stoptNum0, stopNum1 = int(stop[0]), int(stop[1])
+        if startNum0 == stoptNum0:
+            return stopNum1 - startNum1 + 1
+        sum = day[startNum0] - startNum1 + 1 + stopNum1
+        for idx in range(startNum0 + 1, stoptNum0):
+            # print(startNum0, stoptNum0, idx, day[idx])
+            sum += day[idx]
+        return sum
 
 
 su = Solution()
-su.countDaysTogether("08-15", "08-18", "09-16", "09-19")
-su.countDaysTogether("08-15", "09-18", "08-16", "08-19")
-su.countDaysTogether("08-17", "08-18", "08-16", "09-19")
-su.countDaysTogether("08-15", "09-18", "08-16", "09-19")
-su.countDaysTogether("08-15", "09-18", "08-14", "09-16")
+print(su.countDaysTogether("03-05", "07-14", "04-14", "09-21"))
+print(su.countDaysTogether("03-05", "07-14", "07-14", "09-21"))
+print(su.countDaysTogether("01-01", "12-31", "01-01", "12-31"))
